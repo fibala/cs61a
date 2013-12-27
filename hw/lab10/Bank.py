5567ec291904a19636e0098da3ecb4c0afaa47c9
lynn <lynn.exe@gmail.com>
2013-12-27 21:00:40 +0800

    lab10 starter

---
+++ hw/lab10/Bank.py
@@ -0,0 +1,14 @@
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.__balance = 0
+
+    def deposit(self, amount):
+        """YOUR CODE HERE"""
+
+    def withdraw(self, amount):
+        """YOUR CODE HERE"""
+
+    @property
+    def balance(self):
+        return self.__balance
